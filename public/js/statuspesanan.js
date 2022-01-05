f72b8bde6cd92dabfd75b5cece15fb7816297a3f
jerdasi <jeremiadansil@gmail.com>
2022-01-06 06:27:58 +0700

    Fixing Responsive Design

--- public/js/statuspesanan.js
+++ public/js/statuspesanan.js
@@ -51,13 +51,12 @@ const getDiscount = (voucher) => {
 
 // Memunculkan Harga berdasarkan Pilihan
 const priceShow = (idClass, value) => {
-  console.log(idClass, value)
   let hasil = optionTicket.find(o => o.id == value);
   $(`.price${idClass}`).html(hasil.harga);
   checkStatus();
-  pembelian[idClass - 1] = hasil.harga;
+  pembelian[idClass - 1] = hasil.harga; //Menyimpan Harga Tiket yang dipilih untuk dihitung totalnya
   
-  total(); // Memanggil Function Harga
+  total(); // Memanggil Function Total Pembelian
 }
 
 // Mencari Total Pembelian
@@ -92,7 +91,7 @@ const showAndExpire = (dataSet) => {
   waktu = moment(dataSet.data[0].invitation_date).utcOffset(420).format("YYYY-MM-DDTHH:mm:ss") //Mengambil data waktu pengundang dari database
   batas = moment(waktu).add(1, 'd') //Batas Waktu Pengisian adalah 1 hari
   sekarang = moment() //Mengambil jam sekarang saat dipanggil
-
+  
   let minutes = batas.diff(sekarang, 'minutes') % 60 //Modulo untuk mendapatkan sisa menit
   let hours = (batas.diff(sekarang, 'minutes') - minutes) / 60 // Untuk mendapatkan jam
   
@@ -112,6 +111,7 @@ const getData = () => {
   let params = url.searchParams.get('m'); //Mengambil Data dari params keyword 'm'
   let paramsVoucher = url.searchParams.get('voucher_id') //Mengambil Data dari params keyword 'voucher_id'
   getDiscount(paramsVoucher) //Mengambil Besaran Diskon dari Voucher ID
+
   // AJAX jenis Tiket
   $.ajax({
     url: getTicket(paramsVoucher), //Memanggil Function getTicket untuk mendapatkan return IP yang cocok berdasarkan paramter
@@ -119,13 +119,12 @@ const getData = () => {
     dataType: 'json',
     beforeSend: function(){
       getTicketSold()
-      $('#loader').removeClass('d-none')
     },
-    success: function (data, textStatus, xhr) {0
+    success: function (data, textStatus, xhr) {
       data.data.map((item) => {
         console.log(item)
         if(item.ticket_seat != null){ //Jika seat tidak samadengan null
-          if(paramsVoucher == null){ //Jika paramsVoucher adalah null maka akan menampilkan tiket yang tidak memilih voucher_id
+          if(paramsVoucher == null){ //Jika paramsVoucher adalah null maka akan menampilkan tiket yang tidak memiliki voucher_id
             if(item.voucher_id === null && item.ticket_seat != null){
               optionTicket.push({
                 id: item.ticket_id,
@@ -135,7 +134,7 @@ const getData = () => {
                 sisa: item.current_seat
               });
             }
-          } else {
+          } else { //Jika paramsVoucher adalah TIDAK NULL maka akan menampilkan hanya tiket yang memiliki voucher_id
             if(item.voucher_id != null && item.ticket_seat != null){
               optionTicket.push({
                 id: item.ticket_id,
@@ -152,11 +151,10 @@ const getData = () => {
 
       // let panjangOpsi = paramsVoucher != null? optionTicket.length : optionTicket.length; //Mencari Length dari Array Jenis Ticket
       let panjangOpsi = optionTicket.length;
-
-      console.log(panjangOpsi)
       
+      // Jika paramsVoucher adalah null maka akan melakukan kombinasi Tiket berdasarkan tiket yang ada kata "Only"
       if(paramsVoucher == null){
-        let penunjuk = panjangOpsi;
+        let penunjuk = panjangOpsi; //Penunjuk menjadi index untuk ditambahkan tiket jenis baru hasil kombinasi
         let next = 1; //Untuk Value Tiket Gabungan, setiap menambahkan tiket gabungan next + 1, value untuk tiket gabungan adalah banyak tiket yang didapat ditambah next
         for (i = 1; i < panjangOpsi; i++) {
           for (j = i + 1; j < panjangOpsi; j++) {
@@ -180,6 +178,7 @@ const getData = () => {
         }
       }
 
+      // Mengambil data Peserta yang ada dalam satu Pemesanan
       $.ajax({
         url: `http://${ip}/items/invitation?fields=invitation_id,invitation_date,customer_id.customer_email,customer_id.customer_id,customer_id.customer_name,customer_inviter_id.customer_email,invitation_status&filter[customer_inviter_id][customer_code]=${params}`,
         type: 'GET',
@@ -188,55 +187,55 @@ const getData = () => {
           $("#loader").removeClass('d-none');
         },
         success: function (data, textStatus, xhr) {
-
-          showAndExpire(data)
+          showAndExpire(data) //Menampilkan waktu yang tersisa untuk menerima / mengisi biodata bagi peserta yang diundang
           let dataPeserta = data.data
 
           dataPeserta.map((item, index) => {
-            // console.log(item)
             if(item.invitation_status != 2) {
               pembelian.push(0);
               
-              tableRow = `
-                        <tr>
-                            <td>
-                                ${item.customer_id.customer_email}
-                            </td>
-                            <td>${item.customer_id.customer_name == null
-                    ? "Belum Mengisi"
-                    : `${item.customer_id.customer_name}`
-                  }
-                            </td>
-                            ${item.invitation_status == 1 ?
-                    `<td>
-                                <select class="custom-select" id="${index + 1}" name="tiket-peserta-${index + 1}" onchange="priceShow(this.id, this.value)"></select>
-                              </td>
-                              <td class= "price${index + 1}">${0}</td>
-                              <td>
-                                  <div class="card shadow" style="width: 32px; height: 32px; margin: 0 auto;">
-                                  <img src="../public/img/true.svg" alt=""></div>
-                              </td>
-                              <td>
-                                Completed
-                              </td>
-                              `
+              tableRow = 
+              `
+                <tr>
+                    <td>
+                        ${item.customer_id.customer_email}
+                    </td>
+                    <td>
+                      ${item.customer_id.customer_name == null? "Belum Mengisi" : `${item.customer_id.customer_name}`}
+                    </td>
+                    ${item.invitation_status == 1 ?
+                      `<td>
+                          <select class="custom-select" id="${index + 1}" name="tiket-peserta-${index + 1}" onchange="priceShow(this.id, this.value)"></select>
+                        </td>
+                        <td class= "price${index + 1}">${0}</td>
+                        <td>
+                          <div class="card shadow" style="width: 32px; height: 32px; margin: 0 auto;">
+                            <img src="../public/img/true.svg" alt="">
+                          </div>
+                        </td>
+                        <td>
+                          Completed
+                        </td>
+                        `
                     :
                     `<td>
-                                <select class="custom-select" id="${index + 1}" onchange="priceShow(this.id, this.value)" disabled></select></td>
-                              <td class= "price${index + 1}">${0}</td>
-                              <td>
-                                  <div class="card shadow" style="width: 32px; height: 32px; margin: 0 auto;">
-                                  <img src="../public/img/false.svg" alt=""></div>
-                              </td>
-                              <td>
-                                <button class="btn btn-danger" onclick="hapus(${item.invitation_id}, ${item.customer_id.customer_id}, ${2})" type="button">Hapus</button>
-                              </td>
-                              `
-                  }
-                            
-                        </tr>    
-                        `;
+                        <select class="custom-select" id="${index + 1}" onchange="priceShow(this.id, this.value)" disabled></select>
+                      </td>
+                      <td class= "price${index + 1}">${0}</td>
+                      <td>
+                          <div class="card shadow" style="width: 32px; height: 32px; margin: 0 auto;">
+                          <img src="../public/img/false.svg" alt=""></div>
+                      </td>
+                      <td>
+                        <button class="btn btn-danger" onclick="hapus(${item.invitation_id}, ${item.customer_id.customer_id}, ${2})" type="button">Hapus</button>
+                      </td>
+                    `
+                  }            
+                </tr>    
+              `;
+
               $('tbody').append(tableRow);
+              // Menambahkan status pengisian biodata dari setiap peserta
               if (item.invitation_status == 1) {
                 statusPemesanan.push(true);
               } else {
@@ -246,6 +245,7 @@ const getData = () => {
             
           });
 
+          // Menambahkan Option Tiket di Select Box
           optionTicket.map((item, index) => {
             if (optionTicket[index].capacity != null) {
               if (optionTicket[index].capacity == 0) {
