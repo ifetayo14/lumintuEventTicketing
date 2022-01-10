656a4eec3a77ca4ebdf3c490bab6cf36ca279c56
ifetayo14 <alvinsimbolon6@gmail.com>
2022-01-10 10:25:51 +0700

    finishing invitation

--- controller/insertInvitationProcess.php
+++ controller/insertInvitationProcess.php
@@ -19,6 +19,8 @@
 
     $numberOfPost = count($_POST);
 
+    echo $numberOfPost;
+
     $counter = 0;
 
     $curl = curl_init();
@@ -31,46 +33,124 @@
 
     curl_close($curl);
 
+    echo '</br>' . $cred;
+
     $voucherID = 0;
 
-    if (!empty($_POST['voucher'])) {
+    function getVoucher($link, $voucher_code){
         $curl = curl_init();
 
-        curl_setopt($curl, CURLOPT_URL, $voucherURL . '?fields=voucher_id&filter[voucher_code]=' . $_POST['voucher']);
+        curl_setopt($curl, CURLOPT_URL, $link . '?fields=voucher_id&filter[voucher_code]=' . $voucher_code);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($curl);
         $result = json_decode($response, true);
-        $voucherID = $result['data'][0]['voucher_id'];
 
         curl_close($curl);
 
+        return $result;
+    }
+
+    function setInviterData($link, $customerID, $voucherID){
+
+        if ($voucherID == 0) {
+            $payload = '{
+                "customer_id": "' . $customerID . '",
+                "customer_inviter_id": " '. $customerID .' ",
+                "invitation_status": "1"
+            }';
+        }else {
+            $payload = '{
+                "customer_id": "' . $customerID . '",
+                "customer_inviter_id": " '. $customerID .' ",
+                "invitation_status": "1",
+                "voucher_id": "' . $voucherID . '"
+            }';
+        }
+
         $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $invitationURL,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS =>'{
-                        "customer_id": "' . $inviterID . '",
-                        "customer_inviter_id": " '. $inviterID .' ",
-                        "invitation_status": "1",
-                        "voucher_id": "' . $voucherID . '"
-                    }',
-                CURLOPT_HTTPHEADER => array(
-                    'Content-Type: application/json'
-                ),
-            ));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $link,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $payload,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
 
         $getResponse = curl_exec($curl);
         $onCreateResponseInvitation = json_decode($getResponse, true);
 
         curl_close($curl);
 
+        return $onCreateResponseInvitation;
+    }
+
+    function setInvitedData($link, $customerData, $inviterID){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $link,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+            "customer_id": "' . $customerData['data'][0]['customer_id'] . '",
+            "customer_inviter_id": " '. $inviterID .' ",
+            "invitation_status": "0"
+        }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $getResponse = curl_exec($curl);
+        $onCreateResponseInvitation = json_decode($getResponse, true);
+
+        curl_close($curl);
+
+        return $onCreateResponseInvitation;
+    }
+
+    function setCustomer($link, $customerEmail){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $link,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+            "customer_email": "' . $customerEmail . '"
+        }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $getResponse = curl_exec($curl);
+        $onCreateResponseCustomer = json_decode($getResponse, true);
+
+        curl_close($curl);
+
+        return $onCreateResponseCustomer;
+    }
+
+    function sendInviterEmail($receiverEmail, $redirectLink, $cred, $voucher_id){
         $mail = new PHPMailer();
         $mail->SMTPDebug = 0;
         $mail->isSMTP();
@@ -83,75 +163,86 @@
 
         $mail->setFrom('mintuticketing@gmail.com', 'Lumintu Events');
 
-        $mail->addAddress($inviterEmail);
+        $mail->addAddress($receiverEmail);
         $mail->Subject = "[Lumintu Events] Link Pemesanan Tiket";
         $mail->isHTML(true);
-        $mail->Body = 'Hai ' . $resultID['data'][0]['customer_name'] . ', silahkan klik link berikut untuk melakukan pemesanan tiket<br/><br/>
-                        <a href="' . $buyTicketLink . '?m=' . $resultID['data'][0]['customer_code'] . '&voucher_id=' . $voucherID . '">Pesan Tiket</a>';
+        $mailLocation = '../view/email/emailToOrder.html';
+        $message = file_get_contents($mailLocation);
+        $message = str_replace('%inviterMail%', $receiverEmail, $message);
+        if ($voucherID == 0) {
+            $message = str_replace('%link%', $redirectLink . '?m=' . $cred, $message);
+        }else {
+            $message = str_replace('%link%', $redirectLink . '?m=' . $cred . '&voucher_id=' . $voucher_id, $message);
+        }
+        $mail->msgHTML($message);
 
         if ($mail->send()) {
-            header('Location: ../view/details.php?scs');
-        }
-        else {
-            header('Location: ../view/details.php?mailErrSolo');
+            return 'scs';
+        }else {
+            return 'mailErrInviter';
         }
     }
 
+    function sendInvitedEmail($invitedEmail, $inviterEmail, $redirectLink){
+        $mail = new PHPMailer();
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->SMTPSecure = 'tls';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'mintuticketing@gmail.com';
+        $mail->Password = 'Mintu123';
+        $mail->Port = 587;
+
+        $mail->setFrom('mintuticketing@gmail.com', 'Lumintu Events');
+
+        $mail->addAddress($invitedEmail);
+        $mail->Subject = "[Lumintu Events] Link Pengisian Biodata Pemesanan Tiket";
+        $mail->isHTML(true);
+        $mailLocation = '../view/email/emailInvitation.html';
+        $message = file_get_contents($mailLocation);
+        $message = str_replace('%receiverMail%', $invitedEmail, $message);
+        $message = str_replace('%inviterMail%', $inviterEmail, $message);
+        $message = str_replace('%link%', $redirectLink . '?invm=' . base64_encode($invitedEmail), $message);
+        $mail->msgHTML($message);
+
+        if ($mail->send()) {
+            return 'scs';
+        }else {
+            return 'mailErrInvited';
+        }
+
+        $mail->clearAddresses();
+    }
+
+    if (!empty($_POST['voucher'])) {
+
+        $voucherData = getVoucher($voucherURL, $_POST['voucher']);
+        setInviterData($invitationURL, $inviterID, $voucherData['data'][0]['voucher_id']);
+        $sendEmailStatus = sendInviterEmail($inviterEmail, $buyTicketLink, $cred, $voucherID);
+
+        if ($sendEmailStatus == 'scs') {
+            header('Location: ../view/details.php?' . $sendEmailStatus);
+        }else {
+            header('Location: ../view/details.php?' . $sendEmailStatus);
+        }
+
+    }
     elseif ($numberOfPost == 2) {
+
         if ($inviterEmail == $_POST['peserta1']) {
-            $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $invitationURL,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS =>'{
-                        "customer_id": "' . $inviterID . '",
-                        "customer_inviter_id": " '. $inviterID .' ",
-                        "invitation_status": "1",
-                        "voucher_id": "' . $voucherID . '"
-                    }',
-                CURLOPT_HTTPHEADER => array(
-                    'Content-Type: application/json'
-                ),
-            ));
+            setInviterData($invitationURL, $inviterID, $voucherID);
+            $sendEmailStatus = sendInviterEmail($inviterEmail, $buyTicketLink, $cred, $voucherID);
 
-            $getResponse = curl_exec($curl);
-            $onCreateResponseInvitation = json_decode($getResponse, true);
-
-            curl_close($curl);
-
-            $mail = new PHPMailer();
-            $mail->SMTPDebug = 0;
-            $mail->isSMTP();
-            $mail->SMTPSecure = 'tls';
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'mintuticketing@gmail.com';
-            $mail->Password = 'Mintu123';
-            $mail->Port = 587;
-
-            $mail->setFrom('mintuticketing@gmail.com', 'Lumintu Events');
-
-            $mail->addAddress($inviterEmail);
-            $mail->Subject = "[Lumintu Events] Link Pemesanan Tiket";
-            $mail->isHTML(true);
-            $mail->Body = 'Hai ' . $resultID['data'][0]['customer_name'] . ', silahkan klik link berikut untuk melakukan pemesanan tiket<br/><br/>
-                            <a href="' . $buyTicketLink . '?m=' . $resultID['data'][0]['customer_code'] . '&voucher_id=' . $voucherID . '">Pesan Tiket</a>';
-
-            if ($mail->send()) {
-                header('Location: ../view/details.php?scs');
-            } else {
-                header('Location: ../view/details.php?mailErrSolo');
+            if ($sendEmailStatus == 'scs') {
+                header('Location: ../view/details.php?' . $sendEmailStatus);
+            }else {
+                header('Location: ../view/details.php?' . $sendEmailStatus);
             }
+
         }
     }else{
-
         for ($x = 2; $x <= $numberOfPost; $x++){
             if ($_POST['peserta'.$x] != ''){
                 $curl = curl_init();
@@ -169,94 +260,27 @@
         }
 
         if ($counter == 0){
-            $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $invitationURL,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS =>'{
-                        "customer_id": "' . $inviterID . '",
-                        "customer_inviter_id": " '. $inviterID .' ",
-                        "invitation_status": "1"
-                    }',
-                CURLOPT_HTTPHEADER => array(
-                    'Content-Type: application/json'
-                ),
-            ));
-
-            $getResponse = curl_exec($curl);
-            $onCreateResponseInvitation = json_decode($getResponse, true);
-
-            curl_close($curl);
+            setInviterData($invitationURL, $inviterID, $voucherID);
 
             for ($x = 2; $x <= $numberOfPost; $x++){
                 $pesertaEmail = $_POST['peserta'.$x];
-                $curl = curl_init();
+                
+                $getCusStatus = setCustomer($customerURL, $pesertaEmail);
 
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => $customerURL,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'POST',
-                    CURLOPT_POSTFIELDS =>'{
-                    "customer_email": "' . $pesertaEmail . '"
-                }',
-                    CURLOPT_HTTPHEADER => array(
-                        'Content-Type: application/json'
-                    ),
-                ));
-
-                $getResponse = curl_exec($curl);
-                $onCreateResponseCustomer = json_decode($getResponse, true);
-
-                curl_close($curl);
-
-                if (!isset($onCreateResponseCustomer['errors'][0]['extensions']['code'])){
+                if (!isset($getCusStatus['errors'][0]['extensions']['code'])){
                     $curl = curl_init();
-                    curl_setopt($curl, CURLOPT_URL, $customerURL . '?&filter[customer_email]=' . $_POST['peserta'.$x]);
+
+                    curl_setopt($curl, CURLOPT_URL, $customerURL . '?&filter[customer_email]=' . $pesertaEmail);
                     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
                     $responseID = curl_exec($curl);
                     $resultID = json_decode($responseID, true);
 
                     curl_close($curl);
 
-                    $curl = curl_init();
+                    $invitedStatus = setInvitedData($invitationURL, $resultID, $inviterID);
 
-                    curl_setopt_array($curl, array(
-                        CURLOPT_URL => $invitationURL,
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_ENCODING => '',
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 0,
-                        CURLOPT_FOLLOWLOCATION => true,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => 'POST',
-                        CURLOPT_POSTFIELDS =>'{
-                        "customer_id": "' . $resultID['data'][0]['customer_id'] . '",
-                        "customer_inviter_id": " '. $inviterID .' ",
-                        "invitation_status": "0"
-                    }',
-                        CURLOPT_HTTPHEADER => array(
-                            'Content-Type: application/json'
-                        ),
-                    ));
-
-                    $getResponse = curl_exec($curl);
-                    $onCreateResponseInvitation = json_decode($getResponse, true);
-
-                    curl_close($curl);
-
-                    if (!isset($onCreateResponseInvitation['errors'][0]['extensions']['code'])){
+                    if (!isset($invitedStatus['errors'][0]['extensions']['code'])){
 
                     }else{
                         header('Location: ../view/details.php?errOnInv');
@@ -269,51 +293,18 @@
 
             for ($x = 1; $x <= $numberOfPost; $x++){
                 $pesertaEmail = $_POST['peserta'.$x];
-                $mail = new PHPMailer();
-                $mail->SMTPDebug = 0;
-                $mail->isSMTP();
-                $mail->SMTPSecure = 'tls';
-                $mail->Host = 'smtp.gmail.com';
-                $mail->SMTPAuth = true;
-                $mail->Username = 'mintuticketing@gmail.com';
-                $mail->Password = 'Mintu123';
-                $mail->Port = 587;
-
-                $mail->setFrom('mintuticketing@gmail.com', 'Lumintu Events');
-
                 if ($x == 1){
-                    $mail->addAddress($inviterEmail);
-                    $mail->Subject = "[Lumintu Events] Link Pemesanan Tiket";
-                    $mail->isHTML(true);
-//                    $mail->Body = 'Hai ' . $inviterEmail . ', silahkan klik link berikut untuk melakukan pemesanan tiket<br/><br/>
-//                                        <a href="' . $buyTicketLink . '?m=' . $cred .'">Pesan Tiket</a>';
-                    $mailLocation = '../view/email/emailToOrder.html';
-                    $message = file_get_contents($mailLocation);
-                    $message = str_replace('%inviterMail%', $inviterEmail, $message);
-                    $message = str_replace('%link%', $buyTicketLink . '?m=' . $cred, $message);
-                    $mail->msgHTML($message);
+                    $sendEmailStatus = sendInviterEmail($inviterEmail, $buyTicketLink, $cred, $voucherID);
                 }
                 else{
-                    $mail->addAddress($pesertaEmail);
-                    $mail->Subject = "[Lumintu Events] Link Pengisian Biodata Pemesanan Tiket";
-                    $mail->isHTML(true);
-//                    $mail->Body = 'Silahkan klik link berikut untuk melakukan pengisian biodata untuk pemesanan tiket.<br/><br/>
-//                                <a href="' . $bioLink . '?invm=' . base64_encode($pesertaEmail) .'">Pesan Tiket</a>';
-
-                    $mailLocation = '../view/email/emailInvitation.html';
-                    $message = file_get_contents($mailLocation);
-                    $message = str_replace('%receiverMail%', $pesertaEmail, $message);
-                    $message = str_replace('%inviterMail%', $inviterEmail, $message);
-                    $message = str_replace('%link%', $bioLink . '?invm=' . base64_encode($pesertaEmail), $message);
-                    $mail->msgHTML($message);
+                    $mailStatus = sendInvitedEmail($pesertaEmail, $inviterEmail, $bioLink);
                 }
 
-                if ($mail->send()){
+                if ($mailStatus == 'scs'){
                     header('Location: ../view/details.php?allScs');
                 }else{
                     header('Location: ../view/details.php?mailFailed');
                 }
-                $mail->clearAddresses();
             }
         }else{
             header('Location: ../view/details.php?dupEm');
